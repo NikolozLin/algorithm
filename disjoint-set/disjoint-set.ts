6506ce2885fd6720ef38ed23f2aa01f07a489125
linguihao <513279340@qq.com>
2022-01-19 17:47:48 +0800

    update disjoint-set

--- disjoint-set/disjoint-set.ts
+++ disjoint-set/disjoint-set.ts
@@ -1,27 +1,41 @@
-class DisjointSet{
+import { time } from "console"
 
-    // 夫节点
-    public parent: null |DisjointSet
-    public value:string
-    public rank :number //当前节点到根节点的 个数
-    constructor(value:string){
-        this.rank=0
+ class Element{
+    public value:any
+    constructor(value:any){
         this.value=value
-        this.parent=null
+    }
+ }
+ // 使用当前并查集需要 一开始将全部元素添加哦到集合中
+ export class DisjointSet{
+
+    public contentMap:Map<unknown,Element>
+    //记录节点的父节点
+    public fatherMap:Map<Element,Element>
+
+    //表示节点下子节点个数
+    public sizeMap:Map<Element,number>
+
+    constructor(arr:Array<unknown>){
+        this.contentMap= new Map()
+        this.fatherMap= new Map()
+        this.sizeMap= new Map()
+
+        arr.forEach((item:unknown)=>{
+            const eleItem= new Element(time)
+            this.contentMap.set(item,eleItem)
+            this.contentMap.set(eleItem,eleItem)
+            this.sizeMap.set(eleItem,0)
+        })
     }
 
 
-    // 给定集合 添加到当前集合
-    union(set:DisjointSet):boolean{
-        return false
-    }
-
-    //查询给定的 原是 是否在当前集合中
-    find(set:DisjointSet):boolean{
-        
-        
-        
-        return false
+/**
+ * 判断两个节点是否在
+ * @param a 
+ * @param b 
+ */
+    isSameSet(a:Element,b:Element){
 
     }
 
